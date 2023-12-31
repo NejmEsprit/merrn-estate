89c014c93eec652195cd80756b701e1dc4347b0b
NejmEsprit <ammar.nejmeddine@gmail.com>
2023-12-31 22:31:54 +0100

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_CLOUD_URL)
+  .then(() => {
+    console.log('coonect to mongoDB')
+  }).catch((err) => {
+    console.log(err)
+  })
+
 const app = express();
 app.listen(3000, () => {
   console.log("server is raunning on port 3000 !!!!!!!");
